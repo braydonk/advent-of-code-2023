12f85c73e2ee21b2e1535b6043c42508681949fe
Braydon Kains <braydonk@google.com>
2023-12-04 22:11:49 -0500

    Day 4 in Rust (Part 2) (Oh my god it's so slow)

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -8,6 +8,7 @@ use std::{
 
 fn main() {
     run_puzzle_one("./puzzle.txt");
+    run_puzzle_two("./puzzle.txt");
 }
 
 // Copied shamelessly from Rust By Example.
@@ -20,6 +21,7 @@ where P: AsRef<Path>, {
 
 struct Game {
     id: u32,
+    copies: u32,
     winning: HashSet<u32>,
     card: HashSet<u32>
 }
@@ -46,9 +48,27 @@ impl FromStr for Game {
 
         return Ok(Game {
             id: game_id,
+            copies: 1,
             winning: collect_num_line_set(numbers[0]),
             card: collect_num_line_set(numbers[1]),
         });
+    }
+}
+
+impl Game {
+    pub fn score(&self) -> u32 {
+        return self.card.clone().into_iter()
+            .fold(0, |points, number| {
+                if self.winning.contains(&number) {
+                    return if points == 0 { 1 } else { points * 2 };
+                }
+                return points;
+            }
+        );
+    }
+
+    pub fn matching_numbers(&self) -> usize {
+        return self.card.clone().into_iter().filter(|number| self.winning.contains(number)).count();
     }
 }
 
@@ -85,17 +105,37 @@ fn run_puzzle_one<P>(filename: P) where P: AsRef<Path> {
             }
         });
 
-    let _points: u32 = games
-        .map(|game|
-            game.card.into_iter()
-                .fold(0, |points, number| {
-                    if game.winning.contains(&number) {
-                        return if points == 0 { 1 } else { points * 2 };
-                    }
-                    return points;
-                })
-        )
-        .sum();
+    println!("{}", games.map(|game| game.score()).sum::<u32>());
+}
 
-    println!("{}", _points);
+fn run_puzzle_two<P>(filename: P) where P: AsRef<Path> {
+    let lines = read_lines(filename).unwrap();
+
+    let mut games = lines
+        .map(|line| {
+            match line {
+                Ok(game_line) =>
+                    match Game::from_str(game_line.as_str()) {
+                        Ok(game) => game,
+                        Err(_) => panic!("bad line {}", game_line)
+                    }
+                Err(_) => panic!("There was a line we were unable to read."),
+            }
+        })
+        .collect::<Vec<Game>>();
+
+    // Yeah I'm ditching functional style for this part, I'm just not there yet
+    for i in 0_usize..games.len() {
+        for _ in 0..games[i].copies {
+            let matching: usize = games[i].matching_numbers();
+            for n in 1_usize..=matching {
+                if i+n < games.len() {
+                    println!("adding a copy to game {}", games[i+n].id);
+                    games[i+n].copies += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", games.into_iter().fold(0, |sum, game| sum + game.copies));
 }
